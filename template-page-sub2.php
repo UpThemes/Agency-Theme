41cd502aeb846072d3e0236e38c608cc98163187
Matthew Simo <matthew.a.simo@gmail.com>
2012-04-17 15:36:12 -0500

    Add wp formatted pages for 'sub' pages.

---
+++ template-page-sub2.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: Sub page 2
+*/
+?>
+<?php get_header(); ?>
+
+<section class="breadcrumb">
+  <div class="wrap">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li class="seperator">&#9654;</li>
+      <li><a href="#">Page title</a></li>
+    </ul>
+  </div><!--/.wrap-->
+</section><!--/.breadcrumb-->
+<div class="wrap content">
+  <section class="_4-5">
+    <h1>About Us</h1>
+    <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.</p>
+    <h2>Cras dapibus</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.</p>
+    <h3>Aliquam lorem</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.</p>
+  </section>
+  <section class="_1-5">
+    <h2>Agency</h2>
+    <p>
+      555 Main St., Suite 100<br/>
+      San Diego, CA 55555-5454<br/>
+      <br/>
+      <i class="phone"></i>(555) 555-5555<br/>
+      <i class="phone"></i><a href="mailto:info@agency.com">info@agency.com</a>
+    </p>
+
+    <h2>Social Networking</h2>
+    <ul class="social">
+      <li><a href="#"><i class="social-vimeo"></i>Vimeo</a></li>
+      <li><a href="#"><i class="social-twitter"></i>Twitter</a></li>
+      <li><a href="#"><i class="social-linkedin"></i>LinkedIn</a></li>
+      <li><a href="#"><i class="social-forrst"></i>Forrst</a></li>
+      <li><a href="#"><i class="social-flickr"></i>Flickr</a></li>
+      <li><a href="#"><i class="social-facebook"></i>Facebook</a></li>
+      <li><a href="#"><i class="social-dribbble"></i>Dribbble</a></li>
+    </ul>
+  </section>
+</div><!--/.wrap-->
+
+<?php get_footer();?>
