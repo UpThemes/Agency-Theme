49370c204d83f4d601f6598485edc87602c7461a
Matthew Simo <matthew.a.simo@gmail.com>
2012-04-25 09:36:36 -0500

    Pull .js in script tags in the header out and enque it with the rest of the scripts in init.php.

---
+++ assets/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+      $(function(){
+      $('.flexslider').flexslider();
+      $(".breadcrumb").sticky();
+
+      // Create the dropdown base
+      $("<select />").appendTo("header nav");
+
+      // Create default option "Go to..."
+      $("<option />", {
+         "selected": "selected",
+         "value"   : "",
+         "text"    : "Go to..."
+      }).appendTo("nav select");
+
+      // Populate dropdown with menu items
+      $("header nav a").each(function() {
+       var el = $(this);
+       $("<option />", {
+           "value"   : el.attr("href"),
+           "text"    : el.text()
+       }).appendTo("nav select");
+      });
+
+      $("header nav select").change(function() {
+        window.location = $(this).find("option:selected").val();
+      });
+    });
+
+});
--- header.php
+++ header.php
@@ -36,36 +36,6 @@ if (function_exists('upfw_get_options'))
   <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 
-
-  <script type="text/javascript">
-    $(function(){
-      $('.flexslider').flexslider();
-      $(".breadcrumb").sticky();
-
-      // Create the dropdown base
-      $("<select />").appendTo("header nav");
-
-      // Create default option "Go to..."
-      $("<option />", {
-         "selected": "selected",
-         "value"   : "",
-         "text"    : "Go to..."
-      }).appendTo("nav select");
-
-      // Populate dropdown with menu items
-      $("header nav a").each(function() {
-       var el = $(this);
-       $("<option />", {
-           "value"   : el.attr("href"),
-           "text"    : el.text()
-       }).appendTo("nav select");
-      });
-
-      $("header nav select").change(function() {
-        window.location = $(this).find("option:selected").val();
-      });
-    });
-  </script>
 </head>
 <body>
   <header>
