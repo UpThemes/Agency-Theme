a1e0efd9836105aa74fcd7ea1d884e2df77dd1bd
Chris Wallace <chris@walmedia.com>
2012-05-11 13:35:36 -0500

    Cleaned up portfolio template and added content template.

--- single-portfolio.php
+++ single-portfolio.php
@@ -1,10 +1,4 @@
-<?php
-
-get_header();
-
-$up_options = upfw_get_options();
-
-?>
+<?php get_header(); ?>
 
 <?php agency_breadcrumbs(); ?>
 
@@ -17,35 +11,7 @@ $up_options = upfw_get_options();
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-  <section <?php post_class("_4-5 _no-b"); ?>>
-    <h1><?php the_title();?> <?php agency_portfolio_url($post->ID); ?></h1>
-    <div class="viewer-meta">
-      <?php the_terms( $post->ID, 'portfolio_category', '<span class="portfolio-category">', ' - ', '</span>' ); ?>
-      <?php agency_portfolio_launch_date($post->ID); ?>
-    </div>
-  </section>
-  <section class="_1-5">
-    <div class="viewer-controls">
-
-      <?php agency_portfolio_navigation(); ?>
-
-    </div>
-  </section>
-  <hr/>
-
-  <section <?php post_class("_2-3"); ?>>
-    <h3><strong>About</strong></h3>
-    <?php the_content(); ?>
-  </section>
-
-  <section class="_1-3">
-
-
-    <?php agency_portfolio_services($post->ID); ?>
-
-    <?php agency_portfolio_testimonials($post->ID); ?>
-
-  </section>
+	<?php get_template_part( 'portfolio/content', get_post_format() ); ?>
 
 <?php endwhile; ?>
 
