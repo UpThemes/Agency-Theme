cca9aaa8ae610049df125339ffdb485865fc785e
Chris Wallace <chris@walmedia.com>
2012-05-11 23:21:49 -0500

    Added searchform template.

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
+		<input type="text" name="s" id="s" value="<?php get_search_query(); ?>" />
+		<input type="submit" value="Go" id="searchsubmit">
+</form><!--/#searchform-->
