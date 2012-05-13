603c185a5b2cdb27bd1c2439cb5699dee32d7467
Chris Wallace <chris@walmedia.com>
2012-05-13 14:51:17 -0500

    Removed icons from homepage links.

--- template-page-custom-home.php
+++ template-page-custom-home.php
@@ -10,7 +10,7 @@
   <section class="portfolio-module">
     <div class="section-h _1 _no-b">
       <h2><?php _e('Selections from the Portfolio','agency'); ?></h2>
-      <a href="<?php echo home_url('/portfolio/'); ?>"><i class="link"></i><?php _e('view complete portfolio','agency'); ?></a>
+      <a href="<?php echo home_url('/portfolio/'); ?>"><?php _e('view complete portfolio','agency'); ?></a>
     </div><!--/.section-h-->
     <div class="_1 _parent _no-t">
 
@@ -18,16 +18,17 @@
 
     </div>
   </section><!--/.portfolio-module-->
-<hr/>
+
   <section class="blog-module _1-2">
     <div class="section-h">
       <h2><?php _e('Recently on the Blog','agency'); ?></h2>
-      <a href="<?php agency_posts_url(); ?>"><i class="link"></i><?php _e('Read the Blog','agency'); ?></a>
+      <a href="<?php agency_posts_url(); ?>"><?php _e('Read the Blog','agency'); ?></a>
     </div><!--/.section-h-->
 
     <?php agency_blog_home_list(); ?>
 
   </section><!--/.blog-module-->
+
   <section class="testimonial-module _1-2">
     <div class="section-h">
       <h2><?php _e('Testimonials','agency'); ?></h2>
@@ -36,21 +37,22 @@
     <?php agency_testimonial_home_list(); ?>
 
   </section><!--/.testimonial-module-->
-<hr/>
-  <section class="team-module">
 
+  <section class="team-module clear">
+
+    <h2><?php _e("Our Team","agency"); ?>
     <?php agency_team_members_home_list(); ?>
 
   </section><!--/.team-module-->
 
-  <hr/>
-
   <section class="about-module _1-2">
     <?php get_sidebar('home-1'); ?>
   </section><!--/.about-module-->
+
   <section class="office-module _1-2">
     <?php get_sidebar('home-2'); ?>
   </section><!--/.office-module-->
+
 </div><!--/.wrap-->
 
 <?php get_footer(); ?>
