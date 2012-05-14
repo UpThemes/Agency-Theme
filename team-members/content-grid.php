dcd6306be172c309a70b7d12e93d883ae9b329c5
Chris Wallace <chris@walmedia.com>
2012-05-14 14:10:35 -0500

    Removed ratio wrapper from team member images.

--- team-members/content-grid.php
+++ team-members/content-grid.php
@@ -1,15 +1,11 @@
     <section <?php post_class("team-member _1-4"); ?>>
       <?php
-        $post_img =  get_the_post_thumbnail($post->ID, 'responsive',array('class' => 'left'));
+        $post_img =  get_the_post_thumbnail($post->ID, 'portfolio-grid',array('class' => 'left'));
         if ($post_img):
       ?>
-      <div class="ratio-wrap ratio-3-2">
-        <a href="<?php the_permalink(); ?>"><?php echo $post_img; ?></a>
-      </div>
+      <a href="<?php the_permalink(); ?>"><?php echo $post_img; ?></a>
       <?php else: ?>
-      <div class="ratio-wrap ratio-3-2">
-        <a href="<?php the_permalink(); ?>"><?php agency_placeholder('team'); ?></a>
-      </div>
+      <a href="<?php the_permalink(); ?>"><?php agency_placeholder('team'); ?></a>
       <?php endif; ?>
       <h2><a href="<?php the_permalink(); ?>"><?php agency_team_member_name(); ?></a></h2>
       <?php agency_team_member_title(); ?>
