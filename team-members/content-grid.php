60627d22a521126878ee8b098c912007d2b400cf
Chris Wallace <chris@walmedia.com>
2012-05-14 14:23:02 -0500

    Added margin to bottom of team member photos in grid.

--- team-members/content-grid.php
+++ team-members/content-grid.php
@@ -3,9 +3,9 @@
         $post_img =  get_the_post_thumbnail($post->ID, 'portfolio-grid',array('class' => 'left'));
         if ($post_img):
       ?>
-      <a href="<?php the_permalink(); ?>"><?php echo $post_img; ?></a>
+      <a class="team-photo" href="<?php the_permalink(); ?>"><?php echo $post_img; ?></a>
       <?php else: ?>
-      <a href="<?php the_permalink(); ?>"><?php agency_placeholder('team'); ?></a>
+      <a class="team-photo" href="<?php the_permalink(); ?>"><?php agency_placeholder('team'); ?></a>
       <?php endif; ?>
       <h2><a href="<?php the_permalink(); ?>"><?php agency_team_member_name(); ?></a></h2>
       <?php agency_team_member_title(); ?>
