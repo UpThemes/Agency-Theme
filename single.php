0d8aa1fbc143f8ae90dc729db27d7368677cd644
Chris Wallace <chris@walmedia.com>
2012-05-13 19:00:25 -0500

    Fixed layout for single posts.

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 
 <div class="wrap content">
 
-  <section class="_4-5">
+  <section class="_4-5 _parent">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <?php get_template_part( 'content', get_post_format() ); ?>
     <?php endwhile; ?>
