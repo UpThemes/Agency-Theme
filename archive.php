e2d2d26b25555e736f0df8aa561b0719dd639f42
Chris Wallace <chris@walmedia.com>
2012-05-22 17:06:08 -0500

    Fixed sizes on titles, converted font sizes to ems and a few other related bugfixes.

--- archive.php
+++ archive.php
@@ -6,28 +6,22 @@
 
   <section class="_4-5">
 
+    <h1 class="archive-title">
     <?php if ( is_category() ) { ?>
-    <h1>
       <span class="fl cat"><?php _e( 'Category Archive:', 'agency' ); ?> <?php echo single_cat_title(); ?></span>
-    </h1>        
-  
-      <?php } elseif ( is_day() ) { ?>
-      <h1><?php _e( 'Archive', 'agency' ); ?> | <?php the_time( get_option( 'date_format' ) ); ?></h1>
-
+        <?php } elseif ( is_day() ) { ?>
+      <?php _e( 'Archive', 'agency' ); ?> | <?php the_time( get_option( 'date_format' ) ); ?>
       <?php } elseif ( is_month() ) { ?>
-      <h1><?php _e( 'Archive', 'agency' ); ?> | <?php the_time( 'F, Y' ); ?></h1>
-
+      <?php _e( 'Archive', 'agency' ); ?> | <?php the_time( 'F, Y' ); ?>
       <?php } elseif ( is_year() ) { ?>
-      <h1><?php _e( 'Archive', 'agency' ); ?> | <?php the_time( 'Y' ); ?></h1>
-
+      <?php _e( 'Archive', 'agency' ); ?> | <?php the_time( 'Y' ); ?>
       <?php } elseif ( is_author() ) { ?>
-      <h1><?php _e( 'Archive by Author', 'agency' ); ?></h1>
-
+      <?php _e( 'Archive by Author', 'agency' ); ?>
       <?php } elseif ( is_tag() ) { ?>
-      <h1><?php _e( 'Tag Archives:', 'agency' ); ?> <?php echo single_tag_title( '', true); ?></h1>
-      
+      <?php _e( 'Tag Archives:', 'agency' ); ?> <?php echo single_tag_title( '', true); ?>
     <?php } ?>
-
+    </h1>
+  
     <?php if (have_posts()) : ?>
     <div class="clearfix">
     <?php while (have_posts()) : the_post(); ?>
--- content.php
+++ content.php
@@ -4,6 +4,10 @@
 
       <i class="post-icon"></i>
 
+      <div class="meta post-pad-left">
+        <a href="<?php comments_link(); ?>" title="<?php comments_number(__('0 Comments','agency'), __('1 Comment','agency'), __('% Comments','agency') ); ?>" class="comments"><?php comments_number('0', '1', '%'); ?></a> <?php the_category(', '); ?>
+      </div>
+
       <<?php echo $heading; ?> class="post-pad-left"><?php if( !is_singular() ) echo '<a href="' . get_permalink() . '">' ?><?php the_title(); ?><?php if( !is_singular() ) echo '</a>' ?></<?php echo $heading; ?>>
 
       <div class="post-meta post-pad-left">
@@ -12,10 +16,6 @@
 
       <div class="post-pad-left clearfix">
         <?php the_content(); ?>
-      </div>
-
-      <div class="meta post-pad-left">
-        <a href="<?php comments_link(); ?>" title="<?php comments_number(__('0 Comments','agency'), __('1 Comment','agency'), __('% Comments','agency') ); ?>" class="comments"><?php comments_number('0', '1', '%'); ?></a> <?php the_category(', '); ?>
       </div>
 
     </article>
